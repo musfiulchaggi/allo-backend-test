11b0d9e624a98af30454d1347da7b0634e016d57
achmad musfiul chaggi <musfiulchaggi@gmail.com>
2026-03-11 00:55:02 +0700

    feat: implement service for Frankfurter data fetcher

--- src/main/java/com/musfiul/idrrateaggregator/client/FrankfurterClient.java
+++ src/main/java/com/musfiul/idrrateaggregator/client/FrankfurterClient.java
@@ -4,7 +4,7 @@ import com.musfiul.idrrateaggregator.dto.CurrenciesResponse;
 import com.musfiul.idrrateaggregator.dto.HistoricalRatesResponse;
 import com.musfiul.idrrateaggregator.dto.LatestRatesResponse;
 
-interface FrankfurterClient {
+public interface FrankfurterClient {
 
     LatestRatesResponse getLatestRates();
     HistoricalRatesResponse getHistoricalRates();
--- src/main/java/com/musfiul/idrrateaggregator/dto/LatestRatesResponse.java
+++ src/main/java/com/musfiul/idrrateaggregator/dto/LatestRatesResponse.java
@@ -1,6 +1,7 @@
 package com.musfiul.idrrateaggregator.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,4 +16,7 @@ import java.util.Map;
 public class LatestRatesResponse extends BaseResponse<Map<String, BigDecimal>> {
 
     private String date;
+
+    @JsonProperty("USD_BuySpread_IDR")
+    private BigDecimal usdBuySpreadIdr;
 }
