4c427e7b035ed87935a6fe0ffbf38e6e06e2764f
achmad musfiul chaggi <musfiulchaggi@gmail.com>
2026-03-11 07:01:06 +0700

    feat: add logging

--- src/main/java/com/musfiul/idrrateaggregator/runner/FinanceStartupRunner.java
+++ src/main/java/com/musfiul/idrrateaggregator/runner/FinanceStartupRunner.java
@@ -4,10 +4,12 @@ import com.musfiul.idrrateaggregator.constant.ResourceType;
 import com.musfiul.idrrateaggregator.service.FetchResolver;
 import com.musfiul.idrrateaggregator.service.FinanceDataStoreService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class FinanceStartupRunner implements ApplicationRunner {
@@ -18,12 +20,15 @@ public class FinanceStartupRunner implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
 
+        log.info("FinanceStartupRunner");
         for (ResourceType type : ResourceType.values()) {
-
+            log.info("Fetching type : {}", type);
             Object data = resolver
                     .resolve(type)
                     .fetchData();
+            log.info("Fetched data : {}", data);
             store.put(type, data);
         }
+        log.info("FinanceStartupRunner end");
     }
 }
